3812caa98ee3d9c7a3edc782a604e73be926026f
jaggu07 <jagandravid7@gmail.com>
2017-02-25 10:25:40 +0530

    first commit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,61 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
+        "id": "ionic-plugin-keyboard.keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+        "id": "cordova-plugin-x-socialsharing.SocialSharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-console": "1.0.5",
+    "cordova-plugin-device": "1.1.4",
+    "cordova-plugin-splashscreen": "4.0.1",
+    "cordova-plugin-statusbar": "2.2.1",
+    "cordova-plugin-whitelist": "1.3.1",
+    "ionic-plugin-keyboard": "2.2.1",
+    "cordova-plugin-inappbrowser": "1.6.1",
+    "cordova-plugin-x-socialsharing": "5.1.6"
+};
+// BOTTOM OF METADATA
+});
---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,101 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+})
+
+.controller('sourceCntrl', function($scope,$http,$window,$stateParams) {
+ $scope.sourceList = function(){
+    $http.get("https://newsapi.org/v1/sources?language=en").success(function(list){
+   $scope.newsSources = list.sources;
+  })
+ }
+ $scope.checkItems = function(){
+    $scope.CID = [];
+    $scope.name = [];
+    angular.forEach($scope.newsSources,function(src){
+        if(src.checked){
+          $scope.CID.push(src.id);
+          $scope.name.push(src.name);
+          $scope.color='red';
+        }
+    });
+ };
+$scope.display=function(){
+   $window.localStorage.clear();
+var localarray=[];
+var localarrayname=[];
+  for(var i=0;i<$scope.CID.length;i++ ){
+  localarray[i]=$scope.CID[i];
+  localarrayname[i]=$scope.name[i];
+  }
+  localStorage.setItem('data',JSON.stringify(localarray));
+  $scope.output=JSON.parse(localStorage.getItem('data'));
+  localStorage.setItem('dataName',JSON.stringify(localarrayname));
+  $scope.outputname=JSON.parse(localStorage.getItem('dataName'));
+  console.log($scope.outputname);
+}
+})
+.controller('newsCntrl',function($scope,$window,$http){
+  $scope.displaynews=function(){
+  $scope.outputname=JSON.parse(localStorage.getItem('dataName'));
+  $scope.output=JSON.parse(localStorage.getItem('data'));
+  }
+  $scope.content=function(desc){
+   $scope.api=desc;
+    $http.get("https://newsapi.org/v1/articles?source="+$scope.api+"&apiKey=850de99fe1d34a3ea6b92fb4e85c540b").then(function(latestNews){
+    $scope.latestNewslist = latestNews.data.articles;  
+})
+  }
+})
+.controller('descCntrl',function($scope,$window,$stateParams,$ionicModal,$http,$cordovaInAppBrowser,$cordovaSocialSharing){
+//$scope.category=['top','latest','popular']; 
+
+//description view
+    $ionicModal.fromTemplateUrl('templates/desc.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  
+  $scope.openModal = function(arg) {
+    $scope.src=arg;
+    $scope.modal.show();
+  };
+   //Content sharing
+  $scope.shareContent=function(newsImage, description){
+  $cordovaSocialSharing.share('NEWS:', 'subject',newsImage,description) // Share via native share sheet
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occured. Show a message to the user
+    });
+  }
+
+  //In App Browser Opening
+     var options = {
+    location: 'yes',
+    clearcache: 'no',
+    toolbar: 'no'
+  };
+   $scope.openBrowser=function(url){
+      $cordovaInAppBrowser.open(url, '_blank', options)
+      .then(function(event) {
+         success
+      })
+      .catch(function(event) {
+         error
+      });
+    }
+
+
+  $scope.a=function(options)
+  {
+     $http.get("https://newsapi.org/v1/articles?source="+$scope.api+"&sortBy="+options+"&apiKey=850de99fe1d34a3ea6b92fb4e85c540b").then(function(latestNews){
+    $scope.latestNewslist = latestNews.data.articles; 
+  })
+  
+}
+})
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
